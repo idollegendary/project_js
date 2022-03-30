8478bd8d4d4bbef5a5c8fffb8092101cd568508c
kop1s <webdevsense@gmail.com>
2022-03-30 14:20:39 +0300

    lesson 12

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
 "use strict";
 
-console.log('arr' + " - object");
-console.log(4 + "5");
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели', '');
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// ++incr;
-// --decr;
+const a = prompt('один из последних просмотренных фильмов', ''),
+      b = prompt('на сколько оцените его?', ''),
+      c = prompt('один из последних просмотренных фильмов', ''),
+      d = prompt('на сколько оцените его?', '');
 
-console.log(++incr);
-console.log(--decr);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-
-const isChecked = false,
-      isClose = false;
-
-    console.log(isChecked || isChecked);
-
-    
+console.log(personalMovieDB);
